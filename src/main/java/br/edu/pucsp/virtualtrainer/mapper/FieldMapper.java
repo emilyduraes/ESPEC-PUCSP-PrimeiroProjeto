68517bf3977c8a198c1ebd276b1a6249ffeb2b6c
emilyduraes <emilyduraes@yahoo.com.br>
2021-06-01 00:12:36 -0300

    changing fieldMapper

--- src/main/java/br/edu/pucsp/virtualtrainer/mapper/FieldMapper.java
+++ src/main/java/br/edu/pucsp/virtualtrainer/mapper/FieldMapper.java
@@ -8,7 +8,6 @@ import br.edu.pucsp.virtualtrainer.model.entity.Field;
 @Mapper(uses = CategoryMapper.class)
 public interface FieldMapper {
 
-    // TODO: não tá trazendo a categoria
     FieldDto entityToDto(Field field);
     Field dtoToEntity(FieldDto fieldDto);
 }
